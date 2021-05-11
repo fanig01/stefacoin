51d4c1019b54cfd36562da4eb69796a49df9309b
Fagner Rodrigues <fanig_01@hotmail.com>
2021-05-11 20:14:16 -0300

    adicionando rota para cadastro de professor

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import AuthController from '../controllers/auth.controller';
+import ProfessorController from '../controllers/professor.controller';
 import Login from '../models/login.model';
+import BusinessException from '../utils/exceptions/business.exception';
+import Mensagem from '../utils/mensagem';
 
 const router = express.Router();
 
@@ -13,6 +16,23 @@ router.post('/auth', async (req, res, next) => {
     res.json(login);
   } catch (err) {
     next(err);
+  }
+});
+
+router.post('/signup', async (req: Request, res: Response, next: NextFunction) => {
+  try { 
+    let mensagem: Mensagem = <Mensagem>{};
+    switch (req.body.tipo) {
+      case 1:
+        mensagem = await new ProfessorController().incluir(req.body);
+        break;
+    
+      default:
+        throw new BusinessException('Tipo usuário não identificado');
+    }
+    res.json(mensagem);
+  } catch (e) {
+    next(e);
   }
 });
 
